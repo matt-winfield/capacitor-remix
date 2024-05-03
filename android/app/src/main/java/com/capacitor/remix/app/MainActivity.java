939adcf476e6e1a48b91ca0e52db47df3368ae7b
Matthew Winfield <Matthew_Winfield@Derivco.co.uk>
2024-05-03 14:32:04 +0100

    Add some console logs for measuring performance

--- android/app/src/main/java/com/capacitor/remix/app/MainActivity.java
+++ android/app/src/main/java/com/capacitor/remix/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.capacitor.remix.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        long time = System.currentTimeMillis();
+        Log.d("MainActivity", "[PERFORMANCE] onCreate time: " + time);
+    }
+}
--- capacitor/bundled-app/src/pages/home.tsx
+++ capacitor/bundled-app/src/pages/home.tsx
@@ -1,10 +1,17 @@
 import { Capacitor } from "@capacitor/core";
 import { useNavigate } from "react-router-dom";
 import { config } from "../config";
+import { useEffect } from "react";
 
 export const HomePage = () => {
   const navigate = useNavigate();
   const platform = Capacitor.getPlatform();
+
+  useEffect(() => {
+    // Get the current time in milliseconds
+    const currentTime = new Date().getTime();
+    console.log("[PERFORMANCE] Bundled Home Page Loaded at: ", currentTime);
+  }, []);
 
   const goToRemix = () => {
     window.location.href = config.webUrl;
